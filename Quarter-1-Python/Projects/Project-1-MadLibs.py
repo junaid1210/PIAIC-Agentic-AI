4067315bb913e6aa781ddc67e84ff1bb7b229de4
Saim <saimed770@gmai.l.com>
2025-03-09 15:07:40 +0500

    Added new folders and Project 1

---
+++ Quarter-1-Python/Projects/Project-1-MadLibs.py
@@ -0,0 +1,15 @@
+# Assignment number 1: Mad Libs Game
+
+# Getting user input
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+
+# Creating the Mad Libs story
+story = f"One day, a {adjective} {noun} decided to {verb} at the {place}. \
+Everyone was shocked and started laughing."
+
+# Printing the final story
+print("\nHere is your Mad Libs story:", story)
+
